29949bdb8c4e4fa27fce0f0d19619fbb98cca276
Meegu80 <aceman80@nate.com>
2024-12-31 22:54:50 +0900

    Create Variable.java

---
+++ chapter01/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        // 정수형 변수 myNum을 선언하고 15로 초기화
+        int myNum = 15;
+        
+        // 문자열 변수 myName을 선언하고 "John"으로 초기화
+        String myName = "John";
+        
+        // myNum 변수의 값을 콘솔에 출력
+        System.out.println(myNum);
+        
+        // myName 변수의 값을 콘솔에 출력
+        System.out.println(myName);
+    }
+}
